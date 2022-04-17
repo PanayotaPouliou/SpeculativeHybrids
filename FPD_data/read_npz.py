ab3b6ad3c2711f26821864b65c029d1f9efc4a3d
Panayota Pouliou <yotapouliou@gmail.com>
2022-04-17 13:25:44 +0000

    npz files

---
+++ FPD_data/read_npz.py
@@ -0,0 +1,12 @@
+from numpy import load
+
+data = load('statistics_car_train.npz')
+data = load('statistics_chair_train.npz')
+data = load('statistics_guitar_train.npz')
+data = load('statistics_plane_train.npz')
+data = load('statistics_plane_train.npz')
+data = load('statistics_table_train.npz')
+lst = data.files
+for item in lst:
+    print(item)
+    print(data[item])
