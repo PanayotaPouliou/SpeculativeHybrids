f82bfcd591c76ce991c22c02660a08ee7ac163dc
Panayota Pouliou <yotapouliou@gmail.com>
2022-04-26 20:47:16 +0000

    trial of discriminator model cls_model_44

--- FPD_data/read_npz.py
+++ FPD_data/read_npz.py
@@ -1,11 +1,11 @@
 from numpy import load
 
-data = load('statistics_car_train.npz')
-data = load('statistics_chair_train.npz')
-data = load('statistics_guitar_train.npz')
-data = load('statistics_plane_train.npz')
-data = load('statistics_plane_train.npz')
-data = load('statistics_table_train.npz')
+#data = load('statistics_car_train.npz')
+#data = load('statistics_chair_train.npz')
+#data = load('statistics_guitar_train.npz')
+#data = load('statistics_plane_train.npz')
+#data = load('statistics_plane_train.npz')
+data = load('/workspaces/SpeculativeHybrids/FPD_data/statistics_building_train.npz')
 lst = data.files
 for item in lst:
     print(item)
