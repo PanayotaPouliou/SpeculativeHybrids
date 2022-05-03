1ced1ffbc1a1185c774c9d964790ea36fbbc0e6f
Panayota Pouliou <yotapouliou@gmail.com>
2022-05-03 09:13:17 +0000

    2000 epochs run

--- FPD_data/read_npz.py
+++ FPD_data/read_npz.py
@@ -3,8 +3,6 @@ from numpy import load
 #data = load('statistics_car_train.npz')
 #data = load('statistics_chair_train.npz')
 #data = load('statistics_guitar_train.npz')
-#data = load('statistics_plane_train.npz')
-#data = load('statistics_plane_train.npz')
 data = load('/workspaces/SpeculativeHybrids/FPD_data/statistics_building_train.npz')
 lst = data.files
 for item in lst:
